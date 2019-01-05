cf4da53c01dc6a421bf79467756eaab2ec35051e
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-05 18:24:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort{|x, y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort{|left, right| left.length <=> right.length}
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  element = 0
+  new_array = []
+  while counter < array.length
+    new_array << yield(array[element])
+    element += 1
+  end
+  new_array
+end
+
+
+def find_a(array)
+
+end
+
+def sum_array(array)
+
+end
+
+def add_s(array)
+
+end
